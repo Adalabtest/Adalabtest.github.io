210c2a692bc89c4ff2734096712d361c427d3cd2
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 00:07:45 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,3 +3,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");
    // id="smart-image <img src="https://www.adalab.cn/images/adalab.png">；
 });
+targetImage.addEventListener("click", function() {
+    alert("点击了图片");
+});
+targetImage.classList.add("small");
+targetImage.classList.remove("small");
