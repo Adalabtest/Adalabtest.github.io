5d0982f1df31bbed73bef3b7e5ec63e050c060f4
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 19:58:21 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,10 +1,6 @@
 var let ="targetImage";
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
-   // let targetImage = document.querySelector("#smart-image");    
+    let targetImage = document.querySelector("#smart-image");    
 });
-/*targetImage.addEventListener("click", function() {
-   alert("点击了图片");
-    //targetImage.classList.add("small");
-    targetImage.classList.remove("small");
-});*/
+
