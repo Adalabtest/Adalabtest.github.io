88ff93663936bc5ff85799010d4b6241efac9b4e
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 13:45:46 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
-  
+    
 });
+function myFunction()
+{
+    document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
+}
