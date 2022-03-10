b6f9af66f0582ed2d7a206e797a4f5ea863f9890
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 17:37:22 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -5,5 +5,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 targetImage.addEventListener("click", function() {
    alert("点击了图片");
-  targetImage.classList.remove("small");
+    targetImage.classList.add("small");
+//  targetImage.classList.remove("small");
 });
