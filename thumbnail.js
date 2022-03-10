7b6f97842495c9ca8a0193f719ed17afcce9bc1c
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 13:39:27 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
-    <img src="http://static.runoob.com/images/demo/demo1.jpg"class="small">
+   // <img src="http://static.runoob.com/images/demo/demo1.jpg"class="small">
 });
