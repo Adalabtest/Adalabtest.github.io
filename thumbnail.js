e59c799c969d1db9aebcd0f5dcfb533bfbc0b460
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 13:57:12 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
 });
 function myFunction(){
-    
+    <div class="responsive">
+    <div class="img">
+        <a target="_blank" href="//static.runoob.com/images/demo/demo1.jpg">
+        <img src="//static.runoob.com/images/demo/demo1.jpg" alt="图片文本描述" width="300" height="200">
+    </a>
+        <div class="desc">这里添加图片文本描述</div>
     document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
 }
