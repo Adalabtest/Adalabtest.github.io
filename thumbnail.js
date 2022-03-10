f20b69724ad90e08f2b00416ecb1a99ba39e1e15
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 20:19:06 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,6 @@
 var let ="targetImage";
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
+    alert("Hello EVERY");
     let targetImage = document.querySelector("#smart-image");   
     targetImage.addEventListener("click", function() {
         targetImage.classList.add("small");
