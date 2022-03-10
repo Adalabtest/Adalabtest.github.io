3729de16d5beaa120c5c7da64043b53fcedde114
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 20:42:07 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     targetImage.addEventListener("click", function() {
         //targetImage.classList.add("small");
         targetImage.classList.remove("small");
-        alert("Baby!");
+        //alert("Baby!");
 });
 });
 
