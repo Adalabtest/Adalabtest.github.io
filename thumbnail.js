a581a27efeee8a5a056d4adea145e7ba87c5d77b
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-10 11:55:49 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
-    //<img src="http://static.runoob.com/images/demo/demo1.jpg">
-    //class="small";
-    //let targetImage = document.querySelector("#smart-image");
-   // id="smart-image <img src="https://www.adalab.cn/images/adalab.png">；
-//});
-//targetImage.addEventListener("click", function() {
-  //  alert("点击了图片");
-//});
-//targetImage.classList.add("small");
-//targetImage.classList.remove("small");
+  
