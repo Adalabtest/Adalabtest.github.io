a3ccf7c7cbb2f68f9671109c7b1fe7228ba50a76
Adalabtest <101236088+Adalabtest@users.noreply.github.com>
2022-03-11 14:12:08 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,11 +4,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let targetImage = document.querySelector("#smart-image");   
     targetImage.addEventListener("click", function() {
         if (targetImage.classList.contains("small")) {
-                targetImage.classList.remove("small");
+                            targetImage.classList.add("small");
+
+                //targetImage.classList.remove("small");
                // alert("Hello EVERY");
 
             } else {
                 targetImage.classList.add("small");
+                                targetImage.classList.remove("small");
+
                                // alert("Hello Y");
             }
         //targetImage.classList.add("small");
